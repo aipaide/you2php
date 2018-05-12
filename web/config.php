8b4558c9ff5382751cd4dae7f2e8567a2d529940
gfw-breaker <lixiaoming0513@gmail.com>
2018-05-12 08:32:06 +0800

    ok

--- web/config.php
+++ web/config.php
@@ -1,9 +1,10 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY")); //'AIzaSyDOy40AKnOUvotuyMJ8Fk9guYbecB3ZUXo'); //AIzaSyDOy40AKnOUvotuyMJ8Fk9guYbecB3ZUXo');//这里改成您的api key
+define('APIKEY', getenv("APIKEY")); //这里改成您的api key
 define('GJ_CODE', 'TW');//国家代码
 define('SITE_NAME', 'Youtube代理');//网站名字
 define('TITLENAME', 'Youtube代理');//网站名字
 define('EN2DEKEY', 'gfw-breaker');//一个随机字符串，加解密KEY
 define('EMAIL', 'lixiaoming0909@gmail.com');//邮箱
+define('PLAYLIST_ID', getenv("PLAYLIST_ID"));//推荐内容播放列表
 ?>
